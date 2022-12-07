171a5279004bb040ec8552ce59517cb8632d3c4d
yogesh.abnave <yogesh.abnave@otssolutions.com>
2022-12-07 18:18:16 +0530

    git issues fixing

---
+++ admin/src/views/user/TableHead/uesrTableHead/EnhancedTableHead.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import { visuallyHidden } from '@mui/utils';
+import TableHead from '@mui/material/TableHead';
+import TableCell from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Box from '@mui/material/Box';
+import PropTypes from 'prop-types';
+
+export default function EnhancedTableHead(props) {
+    const { order, orderBy, onRequestSort } =
+      props;
+    const createSortHandler = (property) => (event) => {
+      onRequestSort(event, property);
+    };
+    const headCells = [
+        {
+          id: 'fullName',
+          numeric: false,
+          disablePadding: true,
+          label: 'Full Name',
+          headerKey: 'fullName'
+        },
+        {
+          id: 'email',
+          numeric: false,
+          disablePadding: false,
+          label: 'Email',
+          headerKey: 'email'
+        },
+        {
+          id: 'name',
+          numeric: false,
+          disablePadding: false,
+          label: 'Company',
+          headerKey: 'company'
+        },
+        {
+          id: 'range',
+          numeric: false,
+          disablePadding: false,
+          label: 'Employees',
+          headerKey: 'range'
+        },
+        {
+          id: 'e164Number',
+          numeric: false,
+          disablePadding: false,
+          label: 'Mobile',
+          headerKey: 'e164Number'
+        },
+        {
+          id: 'createdAt',
+          numeric: false,
+          disablePadding: false,
+          label: 'Created',
+          headerKey: 'createdAt'
+        },
+        {
+          id: 'active',
+          numeric: false,
+          disablePadding: false,
+          label: 'Status',
+          headerKey: 'active'
+        },
+        {
+          id: 'action',
+          numeric: false,
+          disablePadding: false,
+          label: 'Action',
+          headerKey: 'action'
+        },
+      ];
+   
+    return (
+      <TableHead>
+        <TableRow>
+          {headCells.map((headCell) => (
+            <TableCell
+              key={headCell.id}
+              align={headCell.numeric ? 'right' : 'left'}
+              padding={headCell.disablePadding ? 'none' : 'normal'}
+              sortDirection={orderBy === headCell.id ? order : false}
+              style={{ fontWeight: 'bold', paddingLeft: '25px', whiteSpace: 'nowrap' }}
+            >
+              <TableSortLabel
+                active={orderBy === headCell.headerKey}
+                direction={orderBy === headCell.headerKey ? order : 'asc'}
+                onClick={createSortHandler(headCell.headerKey)}
+              >
+                {headCell.label}
+                {orderBy === headCell.id
+                  ? (<Box component="span" sx={visuallyHidden}>
+                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                  </Box>)
+                  : null}
+              </TableSortLabel>
+            </TableCell>
+          ))}
+        </TableRow>
+      </TableHead>
+    );
+  }
+
+  
+  EnhancedTableHead.propTypes = {
+    numSelected: PropTypes.number.isRequired,
+    onRequestSort: PropTypes.func.isRequired,
+    onSelectAllClick: PropTypes.func.isRequired,
+    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+    orderBy: PropTypes.string.isRequired,
+    rowCount: PropTypes.number.isRequired,
+};
