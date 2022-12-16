d6e6963b78d8de600f5f856eb6195f5222bf20f5
yogesh.abnave <yogesh.abnave@otssolutions.com>
2022-12-16 16:54:33 +0530

    remove input css

--- admin/src/views/subscription-plan/add-plan/add-plan.js
+++ admin/src/views/subscription-plan/add-plan/add-plan.js
@@ -7,7 +7,7 @@ import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import Toolbar from '@mui/material/Toolbar';
 import Paper from '@mui/material/Paper';
-import 'intl-tel-input/build/css/intlTelInput.css';
+// import 'intl-tel-input/build/css/intlTelInput.css';
 import { useHistory } from 'react-router-dom';
 import './add-plan.scss';
 import { UserService } from '../../../shared/_services';
