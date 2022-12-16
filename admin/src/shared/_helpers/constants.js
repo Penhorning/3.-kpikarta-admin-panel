0d092d00834d11b69e24ad0924cacf9eebd93206
sachin.kumar <sachin.kumar@otssolutions.com>
2022-12-16 22:13:05 +0530

    change qa url

--- admin/src/shared/_helpers/constants.js
+++ admin/src/shared/_helpers/constants.js
@@ -1,13 +1,14 @@
 export default {
   // BASE_URL: process.env.API_URL || 'http://localhost:3000'
-  BASE_URL: 'https://dev-api.kpikarta.com'
-    //  BASE_URL: 'https://qa-api.kpikarta.com'
+  // BASE_URL: 'https://dev-api.kpikarta.com'
+     BASE_URL: 'https://qa-api.kpikarta.com'
     //  BASE_URL: 'http://localhost:3000'
 
 }
 
 export const kpikartaUrl = {
-  BASE_URL:'https://dev-app.kpikarta.com'
+  // BASE_URL:'https://dev-app.kpikarta.com'
+  BASE_URL: 'https://qa-api.kpikarta.com'
   // BASE_URL:'https://dev-app.kpikarta.com'
 
  }
