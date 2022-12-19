222fe538bd73bb3598a0d9b45e1a519a7d947521
yogesh.abnave <yogesh.abnave@otssolutions.com>
2022-12-19 22:46:37 +0530

    change url to dev

--- admin/src/shared/_helpers/constants.js
+++ admin/src/shared/_helpers/constants.js
@@ -1,15 +1,14 @@
 export default {
   // BASE_URL: process.env.API_URL || 'http://localhost:3000'
-  // BASE_URL: 'https://dev-api.kpikarta.com'
-     BASE_URL: 'https://qa-api.kpikarta.com'
+  BASE_URL: 'https://dev-api.kpikarta.com'
+    //  BASE_URL: 'https://qa-api.kpikarta.com'
     //  BASE_URL: 'http://localhost:3000'
     // BASE_URL: 'https://prod-api.kpikarta.com'
 }
 
 export const kpikartaUrl = {
-  // BASE_URL:'https://dev-app.kpikarta.com'
-  // BASE_URL:'https://dev-app.kpikarta.com'
+  BASE_URL:'https://dev-app.kpikarta.com'
   // BASE_URL: 'https://prod-app.kpikarta.com'
-    BASE_URL: 'https://qa-app.kpikarta.com'
+    // BASE_URL: 'https://qa-app.kpikarta.com'
  }
 //  BASE_URL: 'https://qa-app.kpikarta.com'
