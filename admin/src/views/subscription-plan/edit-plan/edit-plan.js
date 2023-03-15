ede33d4fb31b5fc3406f533a857e2b9fee19a751
yogesh.abnave <yogesh.abnave@otssolutions.com>
2023-03-15 22:03:06 +0530

    added in sub file

--- admin/src/views/subscription-plan/edit-plan/edit-plan.js
+++ admin/src/views/subscription-plan/edit-plan/edit-plan.js
@@ -14,6 +14,7 @@ import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import 'core-js/actual';
 import './edit-plan.scss';
 import 'react-phone-input-2/lib/material.css';
 
