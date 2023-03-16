29220cde83550510a9c2287eaba7194725e754b2
yogesh.abnave <yogesh.abnave@otssolutions.com>
2023-03-16 16:47:04 +0530

    added node sass

--- admin/src/index.js
+++ admin/src/index.js
@@ -5,17 +5,18 @@ import Spinner from "./views/spinner-loader/spinner-loader";
 import 'core-js/actual';
 import 'globalthis/auto';
 import "./assets/scss/style.scss";
-if (typeof globalThis === 'undefined') {
-  Object.defineProperty(Object.prototype, '__magic__', {
-    get: function() {
-      return this;
-    },
-    configurable: true,
-  });
-  var __magic__ = {};
-  __magic__.globalThis = __magic__;
-  delete Object.prototype.__magic__;
-}
+
+// if (typeof globalThis === 'undefined') {
+//   Object.defineProperty(Object.prototype, '__magic__', {
+//     get: function() {
+//       return this;
+//     },
+//     configurable: true,
+//   });
+//   var __magic__ = {};
+//   __magic__.globalThis = __magic__;
+//   delete Object.prototype.__magic__;
+// }
 
 
 const App = lazy(
